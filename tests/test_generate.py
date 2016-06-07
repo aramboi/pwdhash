5d25bc29dc68b3d7298b8768d13ba57724c5ab74
Angel Ramboi <angel@ramboi.com>
2016-06-07 08:38:43 +0200

    Added tests for subdomains and query params.

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -32,6 +32,34 @@ def test_generate_domain_includes_protocol():
     assert generated == "vYbDjKWA5RoWEWJ"
 
 
+def test_generate_domain_includes_subdomain():
+    domain = "http://bogus.example.com"
+    generated = pwdhash.generate(PASSWORD, domain)
+    assert generated == "0JMBoaKI7NFhObN"
+
+    domain = "https://accounts.google.com"
+    generated = pwdhash.generate(PASSWORD, domain)
+    assert generated == "YzvXOvv3tyZ0PIn"
+
+    domain = "https://www.facebook.com"
+    generated = pwdhash.generate(PASSWORD, domain)
+    assert generated == "vYbDjKWA5RoWEWJ"
+
+
+def test_generate_domain_includes_query_params():
+    domain = "http://bogus.example.com/path?test=test"
+    generated = pwdhash.generate(PASSWORD, domain)
+    assert generated == "0JMBoaKI7NFhObN"
+
+    domain = "https://accounts.google.com/path/?test=test"
+    generated = pwdhash.generate(PASSWORD, domain)
+    assert generated == "YzvXOvv3tyZ0PIn"
+
+    domain = "https://facebook.com/path?test=test"
+    generated = pwdhash.generate(PASSWORD, domain)
+    assert generated == "vYbDjKWA5RoWEWJ"
+
+
 def test_generate_short_password():
     domain = "example.com"
     generated = pwdhash.generate('123', domain)
